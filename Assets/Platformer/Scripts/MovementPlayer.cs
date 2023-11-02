efd39b136b3fb6b9580629dcef4e78e6c301a8ef
Bonart1104 <chshepindn@gmail.com>
2023-11-02 23:22:04 +0700

    Fix

--- Assets/Platformer/Scripts/MovementPlayer.cs
+++ Assets/Platformer/Scripts/MovementPlayer.cs
@@ -1,29 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer))]
+
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+
 public class MovementPlayer : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
     [SerializeField] private Transform _groundChecker;
     [SerializeField] private LayerMask _groundLayer;
-
     [SerializeField] private float _groundRadius = 0.5f;
+
+    private const string Jump = "Jump";
+    private const string Speed = "Speed";
+
     private Animator _animator;
     private float _direction;
     private bool _isForward;
     private bool _isGrounded;
     private Rigidbody2D _rigidbody2D;
     private RaycastHit2D _hit;
+    private SpriteRenderer _spriteRenderer;
 
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -40,7 +45,7 @@ public class MovementPlayer : MonoBehaviour
         {
             _isGrounded = true;
         }
-       
+
 
         if (Input.GetKey(KeyCode.D))
         {
@@ -51,26 +56,25 @@ public class MovementPlayer : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             _isForward = true;
-            Move(_direction, _isForward); 
+            Move(_direction, _isForward);
         }
 
         if (Input.GetKey(KeyCode.Space) && _isGrounded == false)
         {
             _rigidbody2D.AddForce(Vector2.up * _jumpForce);
-            _animator.SetTrigger("Jump");
+            _animator.SetTrigger(Jump);
         }
 
         if (_direction == 0)
         {
-        _animator.SetFloat("Speed", _direction);
-
-        } 
+            _animator.SetFloat(Speed, _direction);
+        }
     }
 
     private void Move(float direction, bool isForward)
     {
         transform.Translate(direction * _speed * Time.deltaTime, 0, 0);
-        GetComponent<SpriteRenderer>().flipX = isForward;
-        _animator.SetFloat("Speed", Mathf.Abs(direction));
+        _spriteRenderer.flipX = isForward;
+        _animator.SetFloat(Speed, Mathf.Abs(direction));
     }
 }
